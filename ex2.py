a50389b04f8eb9308fc4b024cd4e538157de4a2d
basema-k <basema.khan1@ucalgary.ca>
2026-03-16 11:50:46 -0600

    ex 2 part 1

--- ex2.py
+++ ex2.py
@@ -1,17 +1,48 @@
-class Node:
+# exercise 2
+
+# 1:
+
+class TreeNode:
     def __init__(self, item):
         self.data = item
         self.left = None
         self.right = None
 
-def search(root, key):
-    if root is None:
-        return False
-    if root.data == key:
-        return True
-    if key>root.data:
-        return search(root.right, key)
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        # inserts a new key into the binary search tree
+        if self.root is None:
+            self.root = TreeNode(key)
+        else:
+            self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, node, key):
+        if key < node.key:
+            if node.left is None:
+                node.left = TreeNode(key)
+            else:
+                self._insert_recursive(node.left, key)
+        else:
+            if node.right is None:
+                node.right = TreeNode(key)
+            else:
+                self._insert_recursive(node.right, key)
+
+    def search(self, key):
+        # searches for a key in the binary search tree
+        return self._search_recursive(self.root, key)
+
+    def _search_recursive(self, node, key):
+        if node is None or node.key == key:
+            return node is not None  # returns true if found, false otherwise
         
-    return search(root.right, key)
+        if key < node.key:
+            return self._search_recursive(node.left, key)
+        else:
+            return self._search_recursive(node.right, key)
+
     
     
